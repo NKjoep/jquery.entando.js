0d57b7d49551591fecbaed006281efc930b0be3a
NKjoep <nkjoep@gmail.com>
2012-07-25 19:37:57 +0200

    tree sample, improvements

--- jquery.entando.js
+++ jquery.entando.js
@@ -16,19 +16,23 @@
 		toolexpandAllLabelTitle: 'Expand All Tree',	//title for expand  all
 		toolcollapseLabelTitle: 'Collapse All Tree', //title for close all
 		menuRetriever: function(toggler) { return $("#"+toggler.attr("rel")); }
+		//startIndex: ""
+		//onOpen: function(toggler, submenu) {},
+		//onClose: function(toggler, submenu) {},
+		//onStart: function() {},
 	};
 
-	var WoodMenu = function (el, opt) {
-		var Wood = this.Wood = $(el);
+	var Wood = function (el, opt) {
+		var WoodRoot = this.WoodRoot = $(el);
 		var opt = this.opt = opt;
 		var getSubMenu = this.getSubMenu = opt.menuRetriever;
 		var expandAll = this.expandAll = function() {
-			Wood.find("."+opt.menuToggler).each(function(index, toggler){
+			WoodRoot.find("."+opt.menuToggler).each(function(index, toggler){
 				openWood($(toggler));
 			});
 		};
 		var collapseAll = this.collapseAll = function() {
-			Wood.find("."+opt.menuToggler).each(function(index, toggler){
+			WoodRoot.find("."+opt.menuToggler).each(function(index, toggler){
 				closeWood($(toggler));
 			});
 		};
@@ -65,16 +69,21 @@
 				openWood(toggler);
 			}
 		};
+		var openAllWoodFromRootTo = this.openWood = function(toggler) {
+			$(toggler).parentsUntil(WoodRoot).children('.'+opt.menuToggler).each(function(index, togg){
+				openWood($(togg));
+			});
+		};
 
 		if (opt.showTools.toString()=='true') {
-			var toolbar = $('<p class="'+opt.toolClass+'"><span class="intro">'+opt.toolTextIntro+'</span> <a href="#" rel="expand" title="'+opt.toolexpandAllLabelTitle+'">'+opt.expandAllLabel+'</a> <a href="#" rel="collapse" title="'+opt.toolcollapseLabelTitle+'">'+opt.collapseAllLabel+'</a></p>');
+			var toolbar = $('<p class="'+opt.toolClass+'"><span class="'+opt.hideClass+'">'+opt.toolTextIntro+'</span> <a href="#" rel="expand" title="'+opt.toolexpandAllLabelTitle+'">'+opt.expandAllLabel+'</a> <a href="#" rel="collapse" title="'+opt.toolcollapseLabelTitle+'">'+opt.collapseAllLabel+'</a></p>');
 			$(el).before(toolbar);
-			$(toolbar).on('click', 'a[rel="expand"]', {WoodMenu: $(el), WoodMenuOpts: opt}, function(ev) {
+			$(toolbar).on('click', 'a[rel="expand"]', function(ev) {
 				ev.preventDefault();
 				expandAll();
 			});
 
-			$(toolbar).on('click', 'a[rel="collapse"]', {WoodMenu: $(el), WoodMenuOpts: opt}, function(ev) {
+			$(toolbar).on('click', 'a[rel="collapse"]', function(ev) {
 				ev.preventDefault();
 				collapseAll();
 			});
@@ -88,19 +97,27 @@
 		if (opt.onStart) {
 			opt.onStart.apply(this);
 		}
+
+		if (opt.startIndex!==undefined) {
+			openAllWoodFromRootTo(this.WoodRoot.find('#'+opt.startIndex));
+		}
 		return this;
 	};
 
 	$.fn.EntandoWoodMenu = function (userreqopt) {
 		var options = $.extend({},defaultOptions, userreqopt);
-		return this.each(function () {
+		var woodDone = [];
+		this.each(function () {
 			var $this = $(this);
 			var data = $this.data(sign);
-			if (!data) { 
-				$this.data(sign, (data = new WoodMenu(this, options))); 
+			if (!data) {
+				data = new Wood(this, options);
+				$this.data(sign, data);
 			}
-			});
+			woodDone.push(data);
+		});
+		return woodDone;
 	};
 
-	$.fn.EntandoWoodMenu.Constructor = WoodMenu;
+	$.fn.EntandoWoodMenu.Constructor = Wood;
 }(window.jQuery);
